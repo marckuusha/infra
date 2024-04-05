7aae2d0d4d94832500bd7ef422cf25079e1d32e3
Кирилл Марков <marckusha@MacBook-Pro-Kirill.local>
2024-04-05 22:48:25 +0300

    remote server

--- create-network.sh
+++ create-network.sh
@@ -1,2 +1,3 @@
-docker network inspect my_local_network >/dev/null 2>&1 || \
-    docker network create --driver bridge my_local_network
+echo hello world
+# docker network inspect my_local_network >/dev/null 2>&1 || \
+#     docker network create --driver bridge my_local_network
